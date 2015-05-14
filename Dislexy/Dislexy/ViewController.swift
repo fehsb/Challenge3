199e954be31a41fb40c2391510567d824c742884
Fernando Baptista <fernandohsb@gmail.com>
2015-05-14 16:38:29 -0300

    animaçao page control

--- Dislexy/Dislexy/ViewController.swift
+++ Dislexy/Dislexy/ViewController.swift
@@ -9,38 +9,54 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var imgLogo: UIImageView!
     
     @IBOutlet weak var txtView: UITextView!
-   
+    
     @IBOutlet weak var pageControl: UIPageControl!
     
     @IBOutlet weak var backGroundImg: UIImageView!
     
-        
+    
     @IBOutlet weak var viewInvisivel: UIView!
-        
+    
     let texto = ["bla bla bla","ble ble ble","bli bli bli"]
-        
-        
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-            
+        
         txtView.text = texto[pageControl.currentPage]
-            
-            
-            
+        
+        
+        
     }
-        
-        
+    
+    
     @IBAction func swipeDireita(sender: AnyObject) {
-            
+        
         if(pageControl.currentPage < 2){
-                pageControl.currentPage++
-            }
-            update()
+            pageControl.currentPage++
             
+            
+            var posicao = self.txtView.frame.origin.x
+            
+            UIView.animateWithDuration(0.5, animations: { () -> Void in
+                self.txtView.frame.origin.x = -800
+                self.txtView.alpha = 0.5
+                
+                }) { (bool: Bool) -> Void in
+                    self.update()
+                    self.txtView.frame.origin.x = UIScreen.mainScreen().bounds.width + self.txtView.frame.width
+                    
+                    UIView.animateWithDuration(0.5, animations: { () -> Void in
+                        self.txtView.frame.origin.x = posicao
+                        self.txtView.alpha = 1
+                    })
+            }
+        }
+        
     }
     
     
@@ -48,8 +64,24 @@ class ViewController: UIViewController {
         
         if(pageControl.currentPage > 0){
             pageControl.currentPage--
+            
+            var posicao = self.txtView.frame.origin.x
+            
+            UIView.animateWithDuration(0.5, animations: { () -> Void in
+                self.txtView.frame.origin.x = 800
+                self.txtView.alpha = 0.5
+                
+                }) { (bool: Bool) -> Void in
+                    self.update()
+                    self.txtView.frame.origin.x = -self.txtView.frame.width
+                    
+                    UIView.animateWithDuration(0.5, animations: { () -> Void in
+                        self.txtView.frame.origin.x = posicao
+                        self.txtView.alpha = 1
+                        }
+                    )
+            }
         }
-        update()
         
     }
     
